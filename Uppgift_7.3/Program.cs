e4134dec362f0638a3952a4aad614a691362f098
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-02 11:23:36 +0100

    Add project files.

---
+++ Uppgift_7.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Uppgift_7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ländersYtor = new Dictionary<string, int>();
+            ländersYtor["sverige"] = 450;
+            ländersYtor["norge"] = 385;
+            ländersYtor["danmark"] = 43;
+            ländersYtor["finland"] = 338;
+
+            Console.WriteLine("Skriv ett land för att få storleken av det");
+            string inputLand = Console.ReadLine().ToLower();
+
+            if (ländersYtor.ContainsKey(inputLand))
+            {
+                Console.WriteLine($"{inputLand}s yta är {ländersYtor[inputLand]*100000} km^2");
+            }
+            else
+            {
+                Console.WriteLine("Tyvärr, det landet finns inte i listan.");
+            }
+
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
